52095a3e5752417cda0591151a482c15762977c9
Kaluã Bentes <kaluanbentes@gmail.com>
2019-09-15 21:43:52 -0300

    Polish header styles

--- _next/static/Rr2pogCUw-UPy4jQj6neP/pages/index.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[15],{RNiq:function(n,e,u){"use strict";u.r(e);var t=u("mXGw"),i=u.n(t),o=u("Q+If"),w=i.a.createElement;e.default=function(){return w(o.a,null,w("h1",null,"Hello"))}},TqC3:function(n,e,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){var n=u("RNiq");return{page:n.default||n}}])}},[["TqC3",1,0]]]);
