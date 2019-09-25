f6c1c8136a24a53b7ae2542f106654487ed73e6f
Kaluã Bentes <kaluanbentes@gmail.com>
2019-09-24 21:24:20 -0300

    Add favicon

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Head from 'next/head'
 
 import Page from '_templates/page'
 import Cover from '_organisms/cover'
@@ -6,6 +7,9 @@ import Articles from '_organisms/articles'
 
 const Home = () => (
   <Page>
+    <Head>
+      <title>Kaluã Bentes - Frontend Developer</title>
+    </Head>
     <Cover />
     <Articles />
   </Page>
