ef195ceed82b276c679160d550d8474658bcc199
Kaluã Bentes <kaluanbentes@gmail.com>
2019-10-08 13:02:04 -0300

    Update brand style

--- src/components/atoms/brand/index.js
+++ src/components/atoms/brand/index.js
@@ -1,12 +1,29 @@
 import React from 'react'
 import Link from 'next/link'
+import classNames from 'classnames'
+import PropTypes from 'prop-types'
 
 import styles from './styles.css'
 
-const Brand = () => (
+const THEMES = {
+  dark: styles.themeDark,
+  light: styles.themeLight,
+}
+
+const Brand = ({ theme }) => (
   <Link href="/">
-    <div className={styles.brand}>K</div>
+    <div className={classNames(styles.brand, theme)}>Kalux</div>
   </Link>
 )
+
+Brand.propTypes = {
+  theme: PropTypes.string,
+}
+
+Brand.defaultProps = {
+  theme: THEMES.dark,
+}
+
+Brand.themes = THEMES
 
 export default Brand
