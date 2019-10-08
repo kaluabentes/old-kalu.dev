04be926e6fc6078481842c328734413bce0d3dad
Kaluã Bentes <kaluanbentes@gmail.com>
2019-10-08 17:31:16 -0300

    Create page-title component

--- src/components/atoms/page-title/index.js
+++ src/components/atoms/page-title/index.js
@@ -3,4 +3,10 @@ import PropTypes from 'prop-types'
 
 import styles from './styles.css'
 
-const PageTitle = () => <h2></h2>
+const PageTitle = ({ children }) => <h2 className={styles.title}>{children}</h2>
+
+PageTitle.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default PageTitle
