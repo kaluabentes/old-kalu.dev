7df05eec13e87f516905b954706f07047ca1b58d
Kaluã Bentes <kaluanbentes@gmail.com>
2019-10-08 18:14:56 -0300

    Create text-area component

---
+++ src/components/atoms/text-area/index.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
+
+import styles from '_atoms/input/styles.css'
+
+const TextArea = ({
+  id,
+  name,
+  value,
+  errorMessage,
+  label,
+  onChange,
+  isDisabled,
+  hasError,
+}) => (
+  <div>
+    {label && <label className={styles.label} htmlFor={id}>{label}</label>}
+    <textarea
+      className={classNames(
+        styles.input,
+        { [styles.error]: hasError }
+      )}
+      id={id}
+      name={name}
+      onChange={onChange}
+      disabled={isDisabled}
+    >
+      {value}
+    </textarea>
+    {hasError && <div className={styles.errorMessage}>{errorMessage}</div>}
+  </div>
+)
+
+TextArea.propTypes = {
+  className: PropTypes.string,
+  id: PropTypes.string,
+  name: PropTypes.string,
+  value: PropTypes.string,
+  errorMessage: PropTypes.string,
+  label: PropTypes.string,
+  onChange: PropTypes.func,
+  isDisabled: PropTypes.bool,
+  hasError: PropTypes.bool,
+}
+
+TextArea.defaultProps = {
+  className: undefined,
+  id: undefined,
+  name: undefined,
+  value: '',
+  errorMessage: undefined,
+  label: undefined,
+  onChange: () => {},
+  isDisabled: false,
+  hasError: false,
+}
+
+export default TextArea
